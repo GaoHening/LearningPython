9b82da481a0c5f0d1749e4c2ab464590b11c4013
GaoHening <ningningdewang@163.com>
2017-07-17 14:36:12 +0800

    接触python
    使用print和input两个函数

---
+++ hello.py
@@ -0,0 +1,11 @@
+# 输出
+print("hello world!")
+print("The quick brown fox", "jumps over", "the lazy dog")
+print("100+200 =", 100+200)
+
+# 输入输出
+name = input("请输入名字：")
+print("Hello,", name)
+
+# 输出1024 * 768 = xxx
+print("1024 * 768 =", 1024*768)
