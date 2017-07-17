c7ad813246cf8d6e071c8765e0a3c886736ccdb0
GaoHening <ningningdewang@163.com>
2017-07-17 15:26:23 +0800

    学习python基础
    变量和练习

--- basic of python.py
+++ basic of python.py
@@ -13,3 +13,17 @@ line3''')
 
 print(3 > 2)
 
+a = "ABC"
+b = a
+a = "DEF"
+print(b)
+
+# 练习
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(s4)
