a425bb5e77e7d2a30842734dd5602a12d7f786d7
GaoHening <ningningdewang@163.com>
2017-07-18 15:39:47 +0800

    python高级特性
    列表生成器

--- advanced features.py
+++ advanced features.py
@@ -1,3 +1,4 @@
+import os
 L = list(range(100))
 print(L[:10])
 print(L[:10:2])
@@ -11,3 +12,15 @@ for value in d.values():
     print(value)
 for key, value in d.items():
     print(key, ",", value)
+
+la = [x*x for x in range(10)]
+lb = [x*x for x in range(10) if x%2 == 0]
+print(la)
+print(lb)
+
+lc = [d for d in os.listdir('.')]
+print(lc)
+
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [s.lower() for s in L1 if isinstance(s, str)]
+print(L2)
