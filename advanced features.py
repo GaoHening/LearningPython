944d7e875bfc0028beb5c74c9d59b79373ad6d30
GaoHening <ningningdewang@163.com>
2017-07-18 16:19:16 +0800

    python高级特性
    生成器

--- advanced features.py
+++ advanced features.py
@@ -24,3 +24,38 @@ print(lc)
 L1 = ['Hello', 'World', 18, 'Apple', None]
 L2 = [s.lower() for s in L1 if isinstance(s, str)]
 print(L2)
+
+
+def fib(max1):
+    n, a, b = 0, 0, 1
+    while n < max1:
+        yield b
+        a, b, n = b, a+b, n+1
+    return
+
+for n in fib(6):
+    print(n)
+
+
+def triangles():
+    lst = [1]
+    while True:
+        yield lst
+        if lst == [1]:
+            lst = [1, 1]
+        else:
+            time = len(lst) - 1
+            lst_new = [1]
+            number = 1
+            while number <= time:
+                lst_new.append(lst[number-1]+lst[number])
+                number = number + 1
+            lst = lst_new
+            lst.append(1)
+
+n = 0
+for t in triangles():
+    print(t)
+    n = n + 1
+    if n == 10:
+        break
