7d556e8ffc090034c5aa9b96b1036d981379c117
GaoHening <ningningdewang@163.com>
2017-07-18 15:21:53 +0800

    python高级特性
    迭代

--- advanced features.py
+++ advanced features.py
@@ -3,3 +3,11 @@ print(L[:10])
 print(L[:10:2])
 # print(L)
 print("ABCDE"[:2])
+
+d = {'a': 1, 'b': 2, 'c': 3}
+for key in d:
+    print(key)
+for value in d.values():
+    print(value)
+for key, value in d.items():
+    print(key, ",", value)
