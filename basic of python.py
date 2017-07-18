54eb358dd48d7d9ef108b4b2d7d400f3db204741
GaoHening <ningningdewang@163.com>
2017-07-18 10:05:40 +0800

    学习python基础
    list和tuple

--- basic of python.py
+++ basic of python.py
@@ -42,3 +42,19 @@ s2 = 85
 r = (s2-s1)/s1*100
 print("%.1f%%" % r)
 
+classmate = ['A', 'B', 'C']
+print(classmate[len(classmate) - 1])
+classmate.append('D')
+print(classmate)
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+# 打印Apple:
+print(L[0][0])
+# 打印Python:
+print(L[1][1])
+# 打印Lisa:
+print(L[2][2])
