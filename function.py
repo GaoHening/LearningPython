8ac63fffcaa6acb4fec59a275477afc79ec762d9
GaoHening <ningningdewang@163.com>
2017-07-18 13:23:14 +0800

    python函数
    定义函数

--- function.py
+++ function.py
@@ -1,6 +1,23 @@
+import math
 print(abs(-100))
-a = abs
-print(a(-1))
 n1 = 255
 n2 = 1000
 print(hex(n1), hex(n2))
+
+
+def move(x, y, step, angle):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx, ny
+x1, y1 = move(100, 100, 60, math.pi/6)
+print(x1, y1)
+
+
+def quadratic(a, b, c):
+    r1 = (b*(-1)+math.sqrt(b*b-4*a*c))/(2*a)
+    r2 = (b*(-1)-math.sqrt(b*b-4*a*c))/(2*a)
+    return r1, r2
+print(quadratic(2, 3, 1))
+print(quadratic(1, 3, -4))
+
+
--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
 # 输出
 print("hello world!")
 print("The quick brown fox", "jumps over", "the lazy dog")
@@ -9,3 +15,5 @@ print("Hello,", name)
 
 # 输出1024 * 768 = xxx
 print("1024 * 768 =", 1024*768)
+
+
