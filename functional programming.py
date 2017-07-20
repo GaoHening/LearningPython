46a909e56fa8769380e5a92b1181139acf0dd72a
GaoHening <ningningdewang@163.com>
2017-07-20 10:35:10 +0800

    函数式编程
    装饰器

--- functional programming.py
+++ functional programming.py
@@ -152,3 +152,17 @@ def build(x, y):
 f = build(1, 2)
 print(f())
 
+
+def log(func):
+    def wrapper(*args, **kw):
+        print('begin call %s():' % func.__name__)
+        func(*args, **kw)
+        print('end call %s():' % func.__name__)
+        return
+    return wrapper
+
+
+@log
+def now():
+    print('2015-3-25')
+now()
