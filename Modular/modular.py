92ee56c1020ee8c30ea22239edcff5d33fbd08fc
GaoHening <ningningdewang@163.com>
2017-07-20 14:25:55 +0800

    模块
    使用模块

---
+++ Modular/modular.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def text():
+    args = sys.argv
+    if len(args) == 1:
+        print("hello, world")
+    elif len(args) == 2:
+        print("Too many arguments!")
+
+
+def _private_1(name):
+    return 'Hello, %s' % name
+
+
+def _private_2(name):
+    return 'Hi, %s' % name
+
+
+def greeting(name):
+    if len(name) > 3:
+        return _private_1(name)
+    else:
+        return _private_2(name)
+
+if __name__ == '__main__':
+    text()
+    print(greeting('Gao'))
