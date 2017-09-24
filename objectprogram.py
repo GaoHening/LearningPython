c859d9f82b3aafd9158eabc819daac2ad1bd63a0
GaoHening <ningningdewang@163.com>
2017-09-24 13:31:32 +0800

    面向对象编程
    访问限制

--- objectprogram.py
+++ objectprogram.py
@@ -1,10 +1,15 @@
 class Student(object):
     def __init__(self, name, grade):
-        self.name = name
-        self.grade = grade
+        self.__name = name
+        self.__grade = grade
 
     def printgrade(self):
-        print(self.name + "'s grade is " + str(self.grade))
+        print(self.__name + "'s grade is " + str(self.__grade))
+
+    def setgrade(self, grade):
+        self.__grade = grade
 
 Gao = Student("Gao", 95)
+Gao.printgrade()
+Gao.setgrade(80)
 Gao.printgrade()
